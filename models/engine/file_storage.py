ee0f12a9b58c0f7dde9ae9f9efe0c4b23dff59e4
nadertoti <nadertoti4@gmail.com>
2024-01-16 07:24:52 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,45 @@
 #!/usr/bin/python3
-'''FileStorage class file'''
+'''FileStorage class super file'''
 from models.base_model import BaseModel
 import json
 import models
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
-    __file_path = "file.json"
+    '''class for file storage.'''
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        """Return the dictionary __objects."""
+        '''objects for dictionary that will be using.'''
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        '''attribute instance new'''
+        new_value = obj.__class__.__name__ + "." + obj.id
+        self.__objects[new_value] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file (path: __file_path)."""
-        new_dict = {}
+        '''saves to json file'''
+        save_value = {}
         for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
-            json.dump(new_dict, f)
+            save_value[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(save_value, f)
 
     def reload(self):
-        """Deserialize the JSON file to __objects (only if the JSON file (__file_path) exists)."""
+        '''loads from json file without errors'''
         try:
             with open(self.__file_path) as f:
-                new_dict = json.load(f)
+                dict_value = json.load(f)
 
-            for keys in new_dict.keys():
-                self.__objects[keys] = BaseModel(**dic[keys])
+            for keys in dict_value.keys():
+                self.__objects[keys] = BaseModel(**dict_value[keys])
         except FileNotFoundError:
             pass
