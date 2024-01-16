befc718df7307f9b032d6a6d84cc61ea9e52d1d0
nadertoti <nadertoti4@gmail.com>
2024-01-16 03:05:39 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,8 @@ class BaseModel:
             self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """Return string representation of the instance."""
@@ -28,6 +30,7 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary containing all keys/values of __dict__ of the instance."""
